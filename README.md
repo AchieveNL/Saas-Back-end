b128f2a61167ec559575eddc5fd7bfd53d209979
Soumya Mukhopadhyay <soumya.mukhopadhyay@sap.com>
2023-06-03 16:50:55 +0530

    adding a image generation code with Dall e

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Introduction
-This is a backend application that generates images based on user prompts. It uses a machine learning model and a collection of data to create unique images tailored to user input.
+
+This is a backend application that generates images based on user prompts. It uses a machine learning model and a collection of data to create unique images tailored to user input.This application generates only one image based on the user prompt. It uses Open AI Dall-E APIs for Image generation.
+
+## How to use in Local Setup?
+
+1. Install [node js](https://nodejs.org/en/download)
+2. Generate an API KEY at [OpenAI](https://beta.openai.com/) and add it to the `.env` file.
+3. Install the dependencies
+
+```bash
+npm install
+```
+
+* Start the server
+
+```bash
+npm start
+```
+
+* Visit `http://localhost:5000` in your browser.
+* The endpoint is at `POST http://localhost:5000/openai/generateimage`.
--- server.js
+++ server.js
@@ -1,39 +1,17 @@
+const path = require('path');
 const express = require('express');
-const axios = require('axios');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = 3000;
 
+// Enable body parser
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/generate-image', (req, res) => {
-  // Extract the prompt value from the request body
-  const prompt = req.body.prompt;
-  
-  // User prompts
-  const width = 800; // Desired width of the image
-  const height = 600; // Desired height of the image
-  const grayscale = true; // Whether the image should be grayscale or in color
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // Make a request to the Lorem Picsum API
-  axios.get(`https://picsum.photos/${width}/${height}${grayscale ? '?grayscale' : ''}`)
-    .then(response => {
-      const imageUrl = response.request.res.responseUrl;
-      
-      // Construct the response object with the generated image URL and prompt value
-      const imageResponse = {
-        imageUrl,
-        prompt
-      };
-      
-      res.json(imageResponse);
-    })
-    .catch(error => {
-      console.error('Error fetching image from Lorem Picsum:', error);
-      res.status(500).json({ error: 'An error occurred while generating the image.' });
-    });
-});
+app.use('/openai', require('./app/routes/openaiRoutes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
